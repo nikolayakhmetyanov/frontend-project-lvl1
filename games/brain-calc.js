099a8ff785d823126014278afc2b75b6154b4fe7
Nikolay Akhmetyanov <nikolay.akhmetyanov@gmail.com>
2021-10-05 16:34:41 +0300

    create calc game

---
+++ games/brain-calc.js
@@ -0,0 +1,45 @@
+import { askQuastion } from '../src/cli.js';
+
+function brainCalc(name) {
+  const check = (num, answer) => {
+    if (parseInt(answer, 10) === num) {
+      return [true, 'Correct!'];
+    }
+
+    return [false, `'${answer}' is wrong answer ;(. Correct answer was '${num}!`];
+  };
+
+  function getRandomInt() {
+    return Math.floor(Math.random() * 100);
+  }
+
+  function getRandomExpression() {
+    const operation = getRandomInt();
+    const num1 = getRandomInt();
+    const num2 = getRandomInt();
+    const result = {};
+
+    if (operation % 5 === 0) {
+      result.resNum = num1 * num2;
+      result.expression = `${num1} * ${num2}`;
+    } if (operation % 2 === 0) {
+      result.resNum = num1 + num2;
+      result.expression = `${num1} + ${num2}`;
+    } else {
+      result.resNum = num1 - num2;
+      result.expression = `${num1} - ${num2}`;
+    }
+
+    return result;
+  }
+
+  const { resNum, expression } = getRandomExpression();
+
+  console.log(`Quastion: ${expression}`);
+
+  const answer = askQuastion('Your answer: ');
+
+  return check(resNum, answer);
+}
+
+export { brainCalc };
--- makefile
+++ makefile
@@ -1,8 +1,14 @@
-install: 
+install:
 	npm ci
 
 brain-games:
 	node bin/brain-games.js
+
+brain-even:
+	node bin/brain-even.js
+
+brain-calc:
+	node bin/brain-calc.js
 
 publish:
 	npm publish --dry-run
