87a8e0a7759e2e3f2867227c6c0df0449dc60a7b
Nikolay Akhmetyanov <nikolay.akhmetyanov@gmail.com>
2021-10-07 21:14:43 +0300

    fix space

--- src/greetings.js
+++ src/greetings.js
@@ -2,7 +2,7 @@ import askQuastion from './cli.js';
 
 function greetings() {
   console.log('Welcome to the Brain Games!');
-  const name = askQuastion('May I have your name?');
+  const name = askQuastion('May I have your name? ');
   console.log(`Hello, ${name}!`);
 
   return name;
