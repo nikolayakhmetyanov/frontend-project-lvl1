50b631438dbf4433cce5de074b15818adc3da241
Nikolay Akhmetyanov <nikolay.akhmetyanov@gmail.com>
2021-10-07 21:13:13 +0300

    fix greeting message

--- src/greetings.js
+++ src/greetings.js
@@ -2,8 +2,8 @@ import askQuastion from './cli.js';
 
 function greetings() {
   console.log('Welcome to the Brain Games!');
-  const name = askQuastion('May I have your name? ');
-  console.log(`Hello ${name}`);
+  const name = askQuastion('May I have your name?');
+  console.log(`Hello, ${name}!`);
 
   return name;
 }
